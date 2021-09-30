c366e26a3d5a75630612e6eafedcd42302924bce
Thomas Forsberg <tf@kobler.no>
2021-09-30 14:39:35 +0200

    update package.json config

--- server/server.ts
+++ server/server.ts
@@ -1,22 +1,23 @@
 import express from 'express';
-import bodyParser from 'body-parser';
 import {articleList, isArticle} from './articles';
 
 const app = express();
-const PORT = 8000;
+const PORT = 8080;
 
 app.get('/articles', (req, res) => res.json(articleList));
 
-app.post('/articles', bodyParser.json(), (req, res) => {
-  if(!isArticle(req.body)){
+app.post('/articles', (req, res) => {
+  if (!isArticle(req.body)) {
     res.status(422).json({message: 'Incorrect article format'})
-  }else{
+  } else {
     setTimeout(() => {
       articleList.push(req.body)
       res.status(201).json(req.body)
     }, 2500)
   }
 });
+
+app.use(express.json())
 
 app.listen(PORT, () => {
   console.log(`⚡️[server]: Server is running at https://localhost:${PORT}`);
